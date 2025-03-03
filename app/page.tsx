ad01c5e34a9f52c58972243133b3a2a11ab0b66f
Savecoders <pablopincay2003@gmail.com>
2025-03-03 16:15:18 -0500

    chore: refactor component styles for tailwind update

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 bg-slate-100 p-8 pb-20 font-[family-name:var(--font-geist-sans)] dark:bg-zinc-950 sm:p-20">
+    <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 bg-slate-100 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20 dark:bg-zinc-950">
       <main className="row-start-2 flex flex-col items-center gap-8 sm:items-start">
         <h1>Placeholder to page</h1>
       </main>
